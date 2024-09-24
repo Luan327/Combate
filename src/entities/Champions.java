2a5d125a589d56c9116c88b62675f0e5298d3bc5
LuanEscritorio <luanlima747@gmail.com>
2024-09-23 22:56:33 -0300

    criado classe Campeões, getters e setters e construtores

---
+++ src/entities/Champions.java
@@ -0,0 +1,46 @@
+package entities;
+
+public class Champions {
+    private String name;
+    private int life;
+    private int attack;
+    private int armor;
+
+    public Champions(){}
+    public Champions(String name, int life, int attack, int armor) {
+        this.name = name;
+        this.life = life;
+        this.attack = attack;
+        this.armor = armor;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+}
